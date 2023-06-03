543ffebeab9ae91023cab1341303cfc67d0ebb66
razamd <zahid.raza@jazasoft.com>
2023-06-03 21:16:13 +0530

    feat: v7

---
+++ src/hooks/useDeepMemo.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+function useDeepMemo<T>(
+  memoFun: () => T,
+  deps: React.DependencyList | undefined
+) {
+  // use a ref to keep track of deps across renders
+  const depsRef = React.useRef<React.DependencyList>();
+
+  // if the new deps don't deep equal the prev ones
+  // stored in the ref, update the ref. If they *do*
+  // deep equal, the ref will remain unchanged and
+  // the array passed to `useEffect` will be the exact
+  // same array as before so the basic strict equality
+  // will work
+  if (!depsRef || depsRef.current !== deps) {
+    depsRef.current = deps;
+  }
+
+  /* eslint-disable-next-line react-hooks/exhaustive-deps */
+  return React.useMemo(memoFun, depsRef.current);
+}
+
+export default useDeepMemo;
